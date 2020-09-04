499d8e4359c5b3bf3581c4914f1b01adbe977a58
Makid94 <Asus@DESKTOP-I5P9R7H>
2020-09-04 21:49:03 +0600

    First commit

---
+++ EmployeeAPI_Test_Project/src/main/java/TestProjects/EmployeeAPI_Test_Project/post_CreateEmployee.java
@@ -0,0 +1,51 @@
+package TestProjects.EmployeeAPI_Test_Project;
+
+import com.google.gson.JsonObject;
+
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+
+public class post_CreateEmployee {
+	
+	
+	JsonObject json = new JsonObject();
+	JsonPath jsonResponse;
+
+	public void CreateNewEmployee(String name, String salary, String age) {
+		
+		
+		
+		
+		json.addProperty("name", name);
+		json.addProperty("salary", salary);
+		json.addProperty("age", age);
+		
+		RestAssured.baseURI="http://dummy.restapiexample.com";
+		RestAssured.basePath="api/v1/create";
+		
+		jsonResponse = new JsonPath(
+				RestAssured
+				.given()
+				.contentType("application/json")
+				.body(json.toString()).log().all()
+				.when()
+				.post()
+				.asString());
+				
+				jsonResponse.prettyPrint();
+				
+			
+		
+	}
+	
+	public String getEmployeeID() {
+		return jsonResponse.getString("id");
+		
+	}
+	
+	public String getEmployeeName() {
+		return jsonResponse.getString("name");
+		
+	}
+
+}
---
+++ EmployeeAPI_Test_Project/src/test/java/TestProjects/EmployeeAPI_Test_Project/TestCases.java
@@ -0,0 +1,46 @@
+package TestProjects.EmployeeAPI_Test_Project;
+
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.Test;
+
+public class TestCases {
+	
+	
+	get_ViewEmployeeList viewEmployeeList = new get_ViewEmployeeList();
+	get_ViewSingleEmployeeDetail viewEmployeeDetail = new get_ViewSingleEmployeeDetail();
+	post_CreateEmployee createEmployee = new post_CreateEmployee();
+	put_ChangeEmployeeInformation employeeUpdate = new put_ChangeEmployeeInformation();
+	delete_DeleteSingleEmployeeInformation deleteEmployeeByID = new delete_DeleteSingleEmployeeInformation();
+	
+	@Test
+	public void getEmployee_Positive() {
+		viewEmployeeList.getEmployeeList();
+	}
+	
+	@Test
+	public void createEmployee_Positive() {
+		createEmployee.CreateNewEmployee("Leonel messi3", "12344", "32");
+		assertEquals(createEmployee.getEmployeeName(), "Leonel messi3");
+		System.out.println("the employee name is: "+createEmployee.getEmployeeName());
+	}
+	
+	@Test
+	public void getEmployeeDetail() {
+		viewEmployeeDetail.viewingEmployeeDetail("");
+	}
+	
+	@Test
+	public void updateEmployee() {
+		employeeUpdate.updateExistingEmployee("", "", "", "");
+	}
+	
+	@Test
+	public void deleteEmployee() {
+		deleteEmployeeByID.deleteExistingEmployee("");
+	}
+	
+	
+	
+
+}
